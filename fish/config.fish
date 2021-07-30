e2990e668cd223ec30dfcca25b9ae070a26b9103
Robert McGuinness <robert.mcguinness.iii@gmail.com>
2021-07-30 01:30:30 -0400

    refactor: use new fish_add_path

--- fish/config.fish
+++ fish/config.fish
@@ -1,14 +1,12 @@
 . ~/.config/fish/aliases.fish
 set -gx fish_greeting ''
 
-eval (starship init fish)
-
-set -U fish_user_paths /usr/local/bin $fish_user_paths
-set -Ux PYENV_ROOT $HOME/.pyenv
-set -Ux fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-
+set -gx PYENV_ROOT "$HOME/.pyenv"
+fish_add_path -U /usr/local/bin "$PYENV_ROOT/bin" "$HOME/.cargo/bin"
 
 # pyenv init
 if command -v pyenv 1>/dev/null 2>&1
   pyenv init - | source
 end
+
+eval (starship init fish)
