2b357260e8f21601a385b3f0ce6f9fc3f0717bed
Robert McGuinness <robert.mcguinness.iii@gmail.com>
2021-11-09 22:05:00 -0500

    refactor: use fnm for node version manager

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,18 @@
+set fish_greeting
+
+set -x EDITOR "nvim"
+
 fish_add_path /opt/homebrew/bin /opt/homebrew/sbin /usr/local/bin
 
 . ~/.config/fish/aliases.fish
-set -gx fish_greeting ''
 
 starship init fish | source
 zoxide init fish | source
+fnm env | source
 
 set -gx PYENV_ROOT "$HOME/.pyenv"
 fish_add_path -U /usr/local/bin "$PYENV_ROOT/bin" "$HOME/.cargo/bin"
-
 # pyenv init
 status is-interactive; and pyenv init --path | source
+
+
